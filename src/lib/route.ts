8c8f9d9fe8af986c6999a84592676849e095f24e
smpny7 <symphonymode7@icloud.com>
2022-03-19 18:53:13 +0900

    feat: 🎸 経路追加

--- src/lib/route.ts
+++ src/lib/route.ts
@@ -11,14 +11,14 @@ export const route: TransferablePositions[] = [
         underGround: [46],
         bus: [46, 58],
         taxi: [8, 9],
-        boat: [2],
+        boat: [],
     },
     {
         // 2
         underGround: [],
         bus: [],
         taxi: [10, 20],
-        boat: [1],
+        boat: [],
     },
     {
         // 3
@@ -704,6 +704,699 @@ export const route: TransferablePositions[] = [
         underGround: [],
         bus: [63, 82, 111],
         taxi: [80, 81, 101, 112, 113],
+        boat: [],
+    },
+    {
+        // 101
+        underGround: [],
+        bus: [],
+        taxi: [82, 83, 100, 114],
+        boat: [],
+    },
+    {
+        // 102
+        underGround: [],
+        bus: [67, 86, 127],
+        taxi: [83, 103, 115],
+        boat: [],
+    },
+    {
+        // 103
+        underGround: [],
+        bus: [],
+        taxi: [85, 86, 102],
+        boat: [],
+    },
+    {
+        // 104
+        underGround: [],
+        bus: [],
+        taxi: [86, 116],
+        boat: [],
+    },
+    {
+        // 105
+        underGround: [],
+        bus: [72, 89, 107, 108],
+        taxi: [89, 90, 106, 108],
+        boat: [],
+    },
+    {
+        // 106
+        underGround: [],
+        bus: [],
+        taxi: [105, 107],
+        boat: [],
+    },
+    {
+        // 107
+        underGround: [],
+        bus: [72, 161],
+        taxi: [91, 106, 119],
+        boat: [],
+    },
+    {
+        // 108
+        underGround: [],
+        bus: [105, 116, 135],
+        taxi: [105, 117, 119],
+        boat: [115],
+    },
+    {
+        // 109
+        underGround: [],
+        bus: [],
+        taxi: [96, 97, 110, 124],
+        boat: [],
+    },
+    {
+        // 110
+        underGround: [],
+        bus: [],
+        taxi: [98, 99, 109, 111],
+        boat: [],
+    },
+    {
+        // 111
+        underGround: [67, 79, 153, 163],
+        bus: [100, 124],
+        taxi: [110, 112, 124],
+        boat: [],
+    },
+    {
+        // 112
+        underGround: [],
+        bus: [],
+        taxi: [99, 100, 111, 125],
+        boat: [],
+    },
+    {
+        // 113
+        underGround: [],
+        bus: [],
+        taxi: [100, 114, 125],
+        boat: [],
+    },
+    {
+        // 114
+        underGround: [],
+        bus: [],
+        taxi: [101, 113, 126, 131, 132],
+        boat: [],
+    },
+    {
+        // 115
+        underGround: [],
+        bus: [],
+        taxi: [102, 114, 126, 127],
+        boat: [108, 157],
+    },
+    {
+        // 116
+        underGround: [],
+        bus: [86, 108, 127, 142],
+        taxi: [104, 117, 118, 127],
+        boat: [],
+    },
+    {
+        // 117
+        underGround: [],
+        bus: [],
+        taxi: [88, 108, 116, 129],
+        boat: [],
+    },
+    {
+        // 118
+        underGround: [],
+        bus: [],
+        taxi: [116, 129, 134, 142],
+        boat: [],
+    },
+    {
+        // 119
+        underGround: [],
+        bus: [],
+        taxi: [107, 108, 136],
+        boat: [],
+    },
+    {
+        // 120
+        underGround: [],
+        bus: [],
+        taxi: [121, 144],
+        boat: [],
+    },
+    {
+        // 121
+        underGround: [],
+        bus: [],
+        taxi: [120, 122, 145],
+        boat: [],
+    },
+    {
+        // 122
+        underGround: [],
+        bus: [123, 144],
+        taxi: [95, 121, 123, 146],
+        boat: [],
+    },
+    {
+        // 123
+        underGround: [],
+        bus: [122, 124, 144, 165],
+        taxi: [122, 124, 137, 148, 149],
+        boat: [],
+    },
+    {
+        // 124
+        underGround: [],
+        bus: [77, 111, 123, 153],
+        taxi: [109, 111, 123, 130, 138],
+        boat: [],
+    },
+    {
+        // 125
+        underGround: [],
+        bus: [],
+        taxi: [112, 113, 131],
+        boat: [],
+    },
+    {
+        // 126
+        underGround: [],
+        bus: [],
+        taxi: [114, 115, 127, 140],
+        boat: [],
+    },
+    {
+        // 127
+        underGround: [],
+        bus: [102, 116, 133],
+        taxi: [115, 116, 126, 133, 134],
+        boat: [],
+    },
+    {
+        // 128
+        underGround: [89, 140, 185],
+        bus: [135, 142, 161, 187, 199],
+        taxi: [142, 143, 160, 172, 188],
+        boat: [],
+    },
+    {
+        // 129
+        underGround: [],
+        bus: [],
+        taxi: [117, 118, 135, 142, 143],
+        boat: [],
+    },
+    {
+        // 130
+        underGround: [],
+        bus: [],
+        taxi: [124, 131, 139],
+        boat: [],
+    },
+    {
+        // 131
+        underGround: [],
+        bus: [],
+        taxi: [114, 125, 130],
+        boat: [],
+    },
+    {
+        // 132
+        underGround: [],
+        bus: [],
+        taxi: [114, 140],
+        boat: [],
+    },
+    {
+        // 133
+        underGround: [],
+        bus: [127, 140, 157],
+        taxi: [127, 140, 141],
+        boat: [],
+    },
+    {
+        // 134
+        underGround: [],
+        bus: [],
+        taxi: [118, 127, 141, 142],
+        boat: [],
+    },
+    {
+        // 135
+        underGround: [],
+        bus: [108, 128],
+        taxi: [129, 136, 143, 161],
+        boat: [],
+    },
+    {
+        // 136
+        underGround: [],
+        bus: [],
+        taxi: [119, 135, 162],
+        boat: [],
+    },
+    {
+        // 137
+        underGround: [],
+        bus: [],
+        taxi: [123, 147],
+        boat: [],
+    },
+    {
+        // 138
+        underGround: [],
+        bus: [],
+        taxi: [124, 150, 152],
+        boat: [],
+    },
+    {
+        // 139
+        underGround: [],
+        bus: [],
+        taxi: [130, 140, 153, 154],
+        boat: [],
+    },
+    {
+        // 140
+        underGround: [89, 128, 153],
+        bus: [],
+        taxi: [82, 133, 154, 156],
+        boat: [],
+    },
+    {
+        // 141
+        underGround: [],
+        bus: [],
+        taxi: [133, 134, 142, 158],
+        boat: [],
+    },
+    {
+        // 142
+        underGround: [],
+        bus: [116, 128, 157],
+        taxi: [118, 128, 129, 134, 141, 143, 158],
+        boat: [],
+    },
+    {
+        // 143
+        underGround: [],
+        bus: [],
+        taxi: [128, 129, 135, 142, 160],
+        boat: [],
+    },
+    {
+        // 144
+        underGround: [],
+        bus: [122, 123, 163],
+        taxi: [120, 145, 177],
+        boat: [],
+    },
+    {
+        // 145
+        underGround: [],
+        bus: [],
+        taxi: [121, 144, 146],
+        boat: [],
+    },
+    {
+        // 146
+        underGround: [],
+        bus: [],
+        taxi: [122, 145, 147, 163],
+        boat: [],
+    },
+    {
+        // 147
+        underGround: [],
+        bus: [],
+        taxi: [137, 146, 164],
+        boat: [],
+    },
+    {
+        // 148
+        underGround: [],
+        bus: [],
+        taxi: [123, 149, 164],
+        boat: [],
+    },
+    {
+        // 149
+        underGround: [],
+        bus: [],
+        taxi: [123, 148, 150, 165],
+        boat: [],
+    },
+    {
+        // 150
+        underGround: [],
+        bus: [],
+        taxi: [138, 149, 151],
+        boat: [],
+    },
+    {
+        // 151
+        underGround: [],
+        bus: [],
+        taxi: [150, 152, 165, 166],
+        boat: [],
+    },
+    {
+        // 152
+        underGround: [],
+        bus: [],
+        taxi: [138, 151, 153],
+        boat: [],
+    },
+    {
+        // 153
+        underGround: [111, 140, 163, 185],
+        bus: [124, 154, 180, 184],
+        taxi: [139, 152, 154, 166, 167],
+        boat: [],
+    },
+    {
+        // 154
+        underGround: [],
+        bus: [140, 153, 156],
+        taxi: [139, 140, 153, 155],
+        boat: [],
+    },
+    {
+        // 155
+        underGround: [],
+        bus: [],
+        taxi: [154, 156, 167, 168],
+        boat: [],
+    },
+    {
+        // 156
+        underGround: [],
+        bus: [140, 154, 157, 184],
+        taxi: [140, 155, 157, 169],
+        boat: [],
+    },
+    {
+        // 157
+        underGround: [],
+        bus: [133, 142, 156, 185],
+        taxi: [156, 158, 170],
+        boat: [115, 194],
+    },
+    {
+        // 158
+        underGround: [],
+        bus: [],
+        taxi: [141, 142, 157, 159],
+        boat: [],
+    },
+    {
+        // 159
+        underGround: [],
+        bus: [],
+        taxi: [158, 170, 172, 186, 198],
+        boat: [],
+    },
+    {
+        // 160
+        underGround: [],
+        bus: [],
+        taxi: [128, 143, 161, 173],
+        boat: [],
+    },
+    {
+        // 161
+        underGround: [],
+        bus: [107, 128, 199],
+        taxi: [135, 160, 174],
+        boat: [],
+    },
+    {
+        // 162
+        underGround: [],
+        bus: [],
+        taxi: [136, 175],
+        boat: [],
+    },
+    {
+        // 163
+        underGround: [111, 153],
+        bus: [144, 176, 191],
+        taxi: [146, 177],
+        boat: [],
+    },
+    {
+        // 164
+        underGround: [],
+        bus: [],
+        taxi: [147, 148, 178, 179],
+        boat: [],
+    },
+    {
+        // 165
+        underGround: [],
+        bus: [123, 180, 191],
+        taxi: [149, 151, 179, 180],
+        boat: [],
+    },
+    {
+        // 166
+        underGround: [],
+        bus: [],
+        taxi: [151, 153, 181, 183],
+        boat: [],
+    },
+    {
+        // 167
+        underGround: [],
+        bus: [],
+        taxi: [153, 155, 168, 183],
+        boat: [],
+    },
+    {
+        // 168
+        underGround: [],
+        bus: [],
+        taxi: [155, 167, 184],
+        boat: [],
+    },
+    {
+        // 169
+        underGround: [],
+        bus: [],
+        taxi: [156, 184],
+        boat: [],
+    },
+    {
+        // 170
+        underGround: [],
+        bus: [],
+        taxi: [157, 159, 185],
+        boat: [],
+    },
+    {
+        // 171
+        underGround: [],
+        bus: [],
+        taxi: [173, 175, 199],
+        boat: [],
+    },
+    {
+        // 172
+        underGround: [],
+        bus: [],
+        taxi: [128, 159, 187],
+        boat: [],
+    },
+    {
+        // 173
+        underGround: [],
+        bus: [],
+        taxi: [160, 171, 174, 188],
+        boat: [],
+    },
+    {
+        // 174
+        underGround: [],
+        bus: [],
+        taxi: [161, 173, 175],
+        boat: [],
+    },
+    {
+        // 175
+        underGround: [],
+        bus: [],
+        taxi: [162, 171, 174],
+        boat: [],
+    },
+    {
+        // 176
+        underGround: [],
+        bus: [163, 190],
+        taxi: [177, 189],
+        boat: [],
+    },
+    {
+        // 177
+        underGround: [],
+        bus: [],
+        taxi: [144, 163, 176],
+        boat: [],
+    },
+    {
+        // 178
+        underGround: [],
+        bus: [],
+        taxi: [164, 189, 191],
+        boat: [],
+    },
+    {
+        // 179
+        underGround: [],
+        bus: [],
+        taxi: [164, 165, 191],
+        boat: [],
+    },
+    {
+        // 180
+        underGround: [],
+        bus: [153, 165, 184, 190],
+        taxi: [165, 181, 193],
+        boat: [],
+    },
+    {
+        // 181
+        underGround: [],
+        bus: [],
+        taxi: [166, 180, 182, 193],
+        boat: [],
+    },
+    {
+        // 182
+        underGround: [],
+        bus: [],
+        taxi: [181, 183, 195],
+        boat: [],
+    },
+    {
+        // 183
+        underGround: [],
+        bus: [],
+        taxi: [166, 167, 182, 196],
+        boat: [],
+    },
+    {
+        // 184
+        underGround: [],
+        bus: [153, 156, 180, 185],
+        taxi: [168, 169, 185, 196, 197],
+        boat: [],
+    },
+    {
+        // 185
+        underGround: [128, 153],
+        bus: [157, 184, 187],
+        taxi: [170, 184, 186],
+        boat: [],
+    },
+    {
+        // 186
+        underGround: [],
+        bus: [],
+        taxi: [159, 185, 198],
+        boat: [],
+    },
+    {
+        // 187
+        underGround: [],
+        bus: [128, 185],
+        taxi: [172, 188, 198],
+        boat: [],
+    },
+    {
+        // 188
+        underGround: [],
+        bus: [],
+        taxi: [128, 173, 187, 199],
+        boat: [],
+    },
+    {
+        // 189
+        underGround: [],
+        bus: [],
+        taxi: [176, 178, 190],
+        boat: [],
+    },
+    {
+        // 190
+        underGround: [],
+        bus: [176, 180, 191],
+        taxi: [189, 191, 192],
+        boat: [],
+    },
+    {
+        // 191
+        underGround: [],
+        bus: [163, 165, 190],
+        taxi: [178, 179, 190, 192],
+        boat: [],
+    },
+    {
+        // 192
+        underGround: [],
+        bus: [],
+        taxi: [190, 191, 194],
+        boat: [],
+    },
+    {
+        // 193
+        underGround: [],
+        bus: [],
+        taxi: [180, 181, 194],
+        boat: [],
+    },
+    {
+        // 194
+        underGround: [],
+        bus: [],
+        taxi: [192, 193, 195],
+        boat: [157],
+    },
+    {
+        // 195
+        underGround: [],
+        bus: [],
+        taxi: [182, 194, 197],
+        boat: [],
+    },
+    {
+        // 196
+        underGround: [],
+        bus: [],
+        taxi: [183, 184, 197],
+        boat: [],
+    },
+    {
+        // 197
+        underGround: [],
+        bus: [],
+        taxi: [184, 195, 196],
+        boat: [],
+    },
+    {
+        // 198
+        underGround: [],
+        bus: [],
+        taxi: [159, 185, 186, 187, 199],
+        boat: [],
+    },
+    {
+        // 199
+        underGround: [],
+        bus: [128, 161],
+        taxi: [171, 188, 198],
         boat: [],
     },
 ]
